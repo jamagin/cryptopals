874714562d36ba8d61e9a0a6538ccf4a87daa8c0
Alexandra Magin <alexandra@magin.org>
2024-09-02 09:51:38 -0700

    Refactor as traits

--- src/bytes.rs
+++ src/bytes.rs
@@ -15,61 +15,62 @@ fn hex_u8_to_u8(x: u8) -> Result<u8, HexParseError> {
     }
 }
 
-pub fn hex_to_base64(hex_bytes: Vec<u8>) -> Result<Vec<u8>, HexParseError> {
-    const SYMBOLS: [u8; 64] = *b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-    let input = Vec::from_hex_byte_vec(hex_bytes)?;
-    let mut output = vec![0u8; input.len() * 4 / 3];
-    let mut output_pos = 0;
-    for chunk in input.chunks(3) {
-        output[output_pos] = SYMBOLS[(chunk[0] >> 2) as usize];
-        output_pos += 1;
-        output[output_pos] = SYMBOLS[((chunk[0] & 0b11) << 4 | chunk[1] >> 4) as usize];
-        output_pos += 1;
-        output[output_pos] = SYMBOLS[((chunk[1] & 0b1111) << 2 | chunk[2] >> 6) as usize];
-        output_pos += 1;
-        output[output_pos] = SYMBOLS[(chunk[2] & 0b111111) as usize];
-        output_pos += 1;
-    }
-    Ok(output)
-}
-
-
-
-
-
-// Exercise 1-2
-
-
-trait ParseHexByteArray {
+pub trait ParseBytes {
     fn from_hex_byte_vec(src: Vec<u8>) -> Result<Vec<u8>, HexParseError>;
     fn from_hex_byte_array(src: &[u8]) -> Result<Vec<u8>, HexParseError>;
 }
 
-impl ParseHexByteArray for Vec<u8> {
+impl ParseBytes for Vec<u8> {
     fn from_hex_byte_vec(src: Vec<u8>) -> Result<Self, HexParseError> {
         if src.len() % 2 == 1 {
-        return Err(HexParseError);
+            return Err(HexParseError);
+        }
+        
+        let input = src.to_vec();
+        let mut output = Vec::with_capacity(src.len() * 2);
+        for chunk in input.chunks(2) {
+            output.push(hex_u8_to_u8(chunk[0])? << 4 | hex_u8_to_u8(chunk[1])?);
+        }
+        Ok(output)
     }
-
-    let input = src.to_vec();
-    let mut output = Vec::with_capacity(src.len() * 2);
-    for chunk in input.chunks(2) {
-        output.push(hex_u8_to_u8(chunk[0])? << 4 | hex_u8_to_u8(chunk[1])?);
-    }
-    Ok(output)
-}
+    
     fn from_hex_byte_array(src: &[u8]) -> Result<Self, HexParseError> {
         Self::from_hex_byte_vec(src.to_vec())
     }
+}
 
+pub trait RenderBytes {
+    fn as_base64_byte_string(self) -> Result<Vec<u8>, HexParseError>;
+}
+
+impl RenderBytes for Vec<u8> {
+    
+    fn as_base64_byte_string(self) -> Result<Vec<u8>, HexParseError> {
+        const SYMBOLS: [u8; 64] = *b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+        
+        let input = Vec::from_hex_byte_vec(self)?;
+        let mut output = vec![0u8; input.len() * 4 / 3];
+        let mut output_pos = 0;
+        for chunk in input.chunks(3) {
+            output[output_pos] = SYMBOLS[(chunk[0] >> 2) as usize];
+            output_pos += 1;
+            output[output_pos] = SYMBOLS[((chunk[0] & 0b11) << 4 | chunk[1] >> 4) as usize];
+            output_pos += 1;
+            output[output_pos] = SYMBOLS[((chunk[1] & 0b1111) << 2 | chunk[2] >> 6) as usize];
+            output_pos += 1;
+            output[output_pos] = SYMBOLS[(chunk[2] & 0b111111) as usize];
+            output_pos += 1;
+        }
+        Ok(output)
+    }
 
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    
     #[test]
     fn test_hex_u8_to_u8() {
         for x in 0..255 {
@@ -85,19 +86,19 @@ mod tests {
             }
         }
     }
-
+    
+    // Exercise 1-1
     #[test]
     fn test_hex_to_base_64() {
         let hex = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
         let base64 = b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-        assert_eq!(hex_to_base64(hex.to_vec()), Ok(base64.to_vec()));
+        assert_eq!(hex.to_vec().as_base64_byte_string(), Ok(base64.to_vec()));
     }
-
+    
     #[test]
     fn test_from_hex_byte_array() {
         assert_eq!(Vec::from_hex_byte_array(b"123456").unwrap(), vec![0x12u8, 0x34u8, 0x56u8]);
         assert_eq!(Vec::from_hex_byte_array(b"12345"), Err(HexParseError));
-        
     }
 
 }
