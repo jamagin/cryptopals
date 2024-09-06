d8331fad44670c266a2d81c733aa400e1dd152ec
Alexandra Magin <alexandra@magin.org>
2024-09-05 18:35:02 -0700

    reorganize

--- src/frequency.rs
+++ src/frequency.rs
@@ -99,19 +99,4 @@ mod tests {
                 < 0.001
         );
     }
-
-    // Exercise 1-3 solution
-    #[test]
-    fn decrypt_1_3() {
-        let cyphertext = b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-
-        let cypher_bytes = Vec::from_hex_byte_array(cyphertext).unwrap();
-        let (key, text) = break_single_byte_xor(cypher_bytes.clone());
-        println!(
-            "{:x} {:x?} {}",
-            key,
-            text,
-            String::from_utf8(text.clone()).unwrap()
-        );
-    }
 }
